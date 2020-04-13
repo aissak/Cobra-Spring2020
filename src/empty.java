f4ad4eb72a07bcf1b60aeebf0b2cf0f8594004b1
devinkristopher <47421607+devinkristopher@users.noreply.github.com>
2020-04-12 21:24:47 -0400

    adding my sysout in french

--- src/empty.java
+++ src/empty.java
@@ -3,5 +3,6 @@ public class empty {
 	public static void main (String [] args) {
 		System.out.println("HOLA");
 		System.out.println("Hello, hello, hello!");
+		System.out.println("Bonjour!");
 	}
 }
